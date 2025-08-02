b841ba2e108a1ea360ecce60441ec4c0a673a63e
shre-pano-360-tours <josalo@gmail.com>
2025-08-02 21:54:28 +0200

    UPLOAD

---
+++ data.js
@@ -0,0 +1,603 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.839688472991793,
+        "pitch": 0.08333147083440906,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9363794059903725,
+          "pitch": 0.3876747833329368,
+          "rotation": 0,
+          "target": "1-salon-vista-1"
+        },
+        {
+          "yaw": 2.998950639470152,
+          "pitch": 0.15833857565375808,
+          "rotation": 0,
+          "target": "2-salon-vista-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-salon-vista-1",
+      "name": "Salon vista 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.8407144712713492,
+        "pitch": 0.266335760976979,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.34364146103751025,
+          "pitch": 0.4224153321437196,
+          "rotation": 0.7853981633974483,
+          "target": "0-entrada"
+        },
+        {
+          "yaw": 0.49339939874839267,
+          "pitch": 0.48663668256547865,
+          "rotation": 5.497787143782138,
+          "target": "8-distribuidor-2"
+        },
+        {
+          "yaw": 0.655572228008154,
+          "pitch": 0.2666637886991374,
+          "rotation": 7.0685834705770345,
+          "target": "9-bao-2"
+        },
+        {
+          "yaw": 1.0100528406153497,
+          "pitch": 0.23687528252763812,
+          "rotation": 7.0685834705770345,
+          "target": "3-distribuidor-1"
+        },
+        {
+          "yaw": 3.075938642162672,
+          "pitch": 0.38116548590808463,
+          "rotation": 0,
+          "target": "2-salon-vista-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-salon-vista-2",
+      "name": "Salon vista 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.42862606178898766,
+        "pitch": 0.45079465260170615,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3129247397090378,
+          "pitch": 0.44705344460957264,
+          "rotation": 0,
+          "target": "1-salon-vista-1"
+        },
+        {
+          "yaw": 1.2957765279126257,
+          "pitch": 0.20178545106406887,
+          "rotation": 0,
+          "target": "0-entrada"
+        },
+        {
+          "yaw": 1.6916703297026645,
+          "pitch": 0.20831727215057683,
+          "rotation": 0,
+          "target": "8-distribuidor-2"
+        },
+        {
+          "yaw": 1.6838680132958448,
+          "pitch": 0.03509542731449855,
+          "rotation": 0,
+          "target": "10-habitacion"
+        },
+        {
+          "yaw": 1.8486363625883886,
+          "pitch": 0.11258944328986153,
+          "rotation": 1.5707963267948966,
+          "target": "3-distribuidor-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-distribuidor-1",
+      "name": "Distribuidor 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.451886680215477,
+        "pitch": 0.4079617425166919,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7876206449529661,
+          "pitch": 0.392890419074277,
+          "rotation": 5.497787143782138,
+          "target": "1-salon-vista-1"
+        },
+        {
+          "yaw": -1.435367258287009,
+          "pitch": 0.2409083958244551,
+          "rotation": 0.7853981633974483,
+          "target": "0-entrada"
+        },
+        {
+          "yaw": -1.206303092099823,
+          "pitch": 0.6605228012966755,
+          "rotation": 1.5707963267948966,
+          "target": "8-distribuidor-2"
+        },
+        {
+          "yaw": 2.7862228339955415,
+          "pitch": 0.38520550492445693,
+          "rotation": 4.71238898038469,
+          "target": "6-bao-1"
+        },
+        {
+          "yaw": -3.1018250905690827,
+          "pitch": 0.6313461125395143,
+          "rotation": 0,
+          "target": "4-dormitorio-vista-1"
+        },
+        {
+          "yaw": 3.0717125152818374,
+          "pitch": 0.2259087354245537,
+          "rotation": 0,
+          "target": "5-dormitorio-vista-2"
+        },
+        {
+          "yaw": 1.4367767781763945,
+          "pitch": 0.6000715308094087,
+          "rotation": 5.497787143782138,
+          "target": "7-cocina"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dormitorio-vista-1",
+      "name": "Dormitorio vista 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.3324810910991376,
+        "pitch": 0.26132211663210647,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.896351861692696,
+          "pitch": 0.3056526839269331,
+          "rotation": 0,
+          "target": "5-dormitorio-vista-2"
+        },
+        {
+          "yaw": 1.5325849975751469,
+          "pitch": 0.6451583138581771,
+          "rotation": 0,
+          "target": "6-bao-1"
+        },
+        {
+          "yaw": -0.11716244205007342,
+          "pitch": 0.44453265088718297,
+          "rotation": 0,
+          "target": "3-distribuidor-1"
+        },
+        {
+          "yaw": 0.2499169746825487,
+          "pitch": 0.33517469846320935,
+          "rotation": 0.7853981633974483,
+          "target": "7-cocina"
+        },
+        {
+          "yaw": -0.4686809429213099,
+          "pitch": 0.3175449516410964,
+          "rotation": 4.71238898038469,
+          "target": "1-salon-vista-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dormitorio-vista-2",
+      "name": "Dormitorio vista 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.7143873502840927,
+        "pitch": 0.35037245387038496,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5705684962058104,
+          "pitch": 0.3865641625825411,
+          "rotation": 1.5707963267948966,
+          "target": "6-bao-1"
+        },
+        {
+          "yaw": 1.4154043142485193,
+          "pitch": 0.4451060708314909,
+          "rotation": 0,
+          "target": "4-dormitorio-vista-1"
+        },
+        {
+          "yaw": 1.4828196301903205,
+          "pitch": 0.28549731910610276,
+          "rotation": 0,
+          "target": "3-distribuidor-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-bao-1",
+      "name": "Baño 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.1049541600703154,
+        "pitch": 0.3780007268998027,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8530998928911133,
+          "pitch": 0.3737329763203867,
+          "rotation": 11.780972450961727,
+          "target": "4-dormitorio-vista-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-cocina",
+      "name": "Cocina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.5708547478253383,
+        "pitch": 0.258722431486877,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0910931575620104,
+          "pitch": 0.5773149437937501,
+          "rotation": 0,
+          "target": "3-distribuidor-1"
+        },
+        {
+          "yaw": 0.8978854746219387,
+          "pitch": 0.353811384445919,
+          "rotation": 4.71238898038469,
+          "target": "4-dormitorio-vista-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-distribuidor-2",
+      "name": "Distribuidor 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.29105233086144544,
+        "pitch": 0.1397657040890259,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.813038801514704,
+          "pitch": 0.4526985806799786,
+          "rotation": 0.7853981633974483,
+          "target": "1-salon-vista-1"
+        },
+        {
+          "yaw": -2.9682369928299686,
+          "pitch": 0.19817959773173754,
+          "rotation": 0,
+          "target": "2-salon-vista-2"
+        },
+        {
+          "yaw": 2.6773794186052307,
+          "pitch": 0.5669816679630575,
+          "rotation": 4.71238898038469,
+          "target": "3-distribuidor-1"
+        },
+        {
+          "yaw": 1.598335754886425,
+          "pitch": 0.6674431834627761,
+          "rotation": 0,
+          "target": "9-bao-2"
+        },
+        {
+          "yaw": -0.010236255163466268,
+          "pitch": 0.476082232707034,
+          "rotation": 0,
+          "target": "10-habitacion"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-bao-2",
+      "name": "Baño 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.6987456891586366,
+        "pitch": 0.5889585792613445,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.521001931838228,
+          "pitch": 0.4369196377009228,
+          "rotation": 0,
+          "target": "8-distribuidor-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-habitacion",
+      "name": "Habitacion",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.4855188646456945,
+        "pitch": 0.3478671232262016,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1150056257486547,
+          "pitch": 0.5470276852452649,
+          "rotation": 0,
+          "target": "8-distribuidor-2"
+        },
+        {
+          "yaw": -3.0888487157829783,
+          "pitch": 0.27577093125348817,
+          "rotation": 0.7853981633974483,
+          "target": "2-salon-vista-2"
+        },
+        {
+          "yaw": 3.0312213084028894,
+          "pitch": 0.23295603860715808,
+          "rotation": 4.71238898038469,
+          "target": "3-distribuidor-1"
+        },
+        {
+          "yaw": 2.951579049333189,
+          "pitch": 0.45987827669972603,
+          "rotation": 4.71238898038469,
+          "target": "9-bao-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "202508_28010_Z5",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
